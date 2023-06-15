57dd3a0d751394130ed6c529de2fea43265cefa4
BLACKHENZ29 <ayehenz29@gmail.com>
2023-06-15 08:19:47 +0100

    initialization of project

---
+++ app.js
@@ -0,0 +1,34 @@
+import express  from 'express';
+import dotenv from 'dotenv'
+import cors from 'cors'
+dotenv.config();
+const app = express();
+app.use(express.json())
+
+app.use(cors({
+    origin: '*'
+}
+))
+app.options('*', cors())
+app.use(
+    cors({
+        credentials: true,
+        origin: true,
+        allowedHeaders: "*"
+    })
+)
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    );
+    next();
+});
+
+const PORT = process.env.PORT || 2000
+app.listen(PORT,console.log(`Server is running at ${PORT}`))
