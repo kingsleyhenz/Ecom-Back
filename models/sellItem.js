9c16dcae37bd988a96bb5ca682c7d4a115713517
BLACKHENZ29 <ayehenz29@gmail.com>
2023-07-09 20:29:06 +0100

    User Selling Model

---
+++ models/sellItem.js
@@ -0,0 +1,50 @@
+import mongoose from mongoose;
+
+const sellSchema = new mongoose.Schema({
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+    category:{
+        type: String,
+        required: true,
+    },
+    SubCategory:{
+        type: String,
+        required: true,
+    },
+    Model:{
+        type: String,
+        required: true,
+    },
+    specs:[{
+        type: String,
+    }],
+    price:{ 
+        type: Number,
+        required: true,
+    },
+    description:{
+        type: String,
+        required: true,
+    },
+    imageUrl:{
+        type: String,
+        required: true,
+    },
+    quantity:{
+        type: Number,
+        default: 1,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: function() {
+          return this._createdAt.toLocaleDateString('en-US');
+        }
+      }
+}) 
+
+const sellItem = mongoose.model("sellItem", sellSchema);
+
+export default sellItem
