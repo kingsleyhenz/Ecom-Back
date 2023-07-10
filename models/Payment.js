1b52382f96b79ad7ac9efea6162765b2a60ab0f8
BLACKHENZ29 <ayehenz29@gmail.com>
2023-07-10 12:14:16 +0100

    Payment tweaking

--- models/Payment.js
+++ models/Payment.js
@@ -1,18 +1,39 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const paymentMethodSchema = new mongoose.Schema({
-  paymentType: {
-    type: String,
-    enum: ["Paystack", "Stripe"],
+const paymentSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
     required: true,
   },
-  paymentStatus: {
+  order: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Order',
+    required: true,
+  },
+  paymentMethod: {
     type: String,
-    enum: ['Successful', 'Pending', 'Failed'],
-    default: 'Pending',
+    required: true,
+  },
+  paymentReference: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ['success', 'failed', 'pending'],
+    default: 'pending',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
   },
 });
 
-const PaymentMethod = mongoose.model("PaymentMethod", paymentMethodSchema);
+const Payment = mongoose.model('Payment', paymentSchema);
 
-export default PaymentMethod;
+export default Payment;
