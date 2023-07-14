1457871c345baffb7a1874195c19bbee079212b7
BLACKHENZ29 <ayehenz29@gmail.com>
2023-07-14 22:18:15 +0100

    Get Canceled Orders

--- controller/admin.js
+++ controller/admin.js
@@ -246,6 +246,26 @@ export const getDeliveredOrders = async (req, res) => {
     })
   }
 }
+
+export const getCanceledOrders = async (req, res) => {
+  if(!req.userAuth){
+    res.status(401).json({ message: 'Unauthorized' })
+  }
+  try {
+    const orders = await Order.find({ deliveryStatus: 'Canceled'})
+    res.json({
+      status: 'success',
+      data: orders
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      status: 'error',
+      message: 'Failed to get Orders'
+    })
+  }
+}
+
 export const makeAdmin = async(req,res)=>{
   if(!req,userAuth){
     req.status(401).json({ message: 'Unauthorized' })
